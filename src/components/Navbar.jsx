e1b3442ba7d7b71b8098d592a4bd224258893873
HoangNhat2004 <nhnhat202@gmail.com>
2025-11-16 14:35:52 +0700

    Fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,8 +36,6 @@ const Navbar = () => {
             setSearchQuery("")
         }
     }
-
-    const token = localStorage.getItem('token');
   
     return (
         <header className="max-w-screen-2xl mx-auto px-4 py-6 border-b border-subtle"> {/* Thêm border */}
@@ -70,38 +68,42 @@ const Navbar = () => {
                 <div className="relative flex items-center md:space-x-3 space-x-2">
                     <div >
                         {
-                            currentUser ? <>
-                            <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
-                                <img 
-                                    src={currentUser.photoURL || avatarImg} 
-                                    alt="User avatar" 
-                                    className={`size-8 rounded-full ${currentUser ? 'ring-2 ring-accent' : ''}`} // Sửa màu ring
-                                />
-                            </button>
-                            {/* show dropdowns */}
-                            {
-                                isDropdownOpen && (
-                                    <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
-                                        <ul className="py-2">
-                                            {
-                                                navigation.map((item) => (
-                                                    <li key={item.name} onClick={() => setIsDropdownOpen(false)}>
-                                                        <Link to={item.href} className="block px-4 py-2 text-sm hover:bg-gray-100">
-                                                            {item.name}
-                                                        </Link>
-                                                    </li>
-                                                ))
-                                            }
-                                            <li>
-                                                <button
-                                                onClick={handleLogOut}
-                                                className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100">Logout</button>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                )
-                            }
-                            </> : token ?  <Link to="/dashboard" className='border-b-2 border-primary'>Dashboard</Link> : (
+                            currentUser ? (
+                                // Nếu CÓ currentUser (bất kể là Google hay JWT)
+                                <>
+                                <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                                    <img 
+                                        src={currentUser.photoURL || avatarImg} 
+                                        alt="User avatar" 
+                                        className={`size-8 rounded-full ${currentUser ? 'ring-2 ring-accent' : ''}`} // Sửa màu ring
+                                    />
+                                </button>
+                                {/* show dropdowns */}
+                                {
+                                    isDropdownOpen && (
+                                        <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                                            <ul className="py-2">
+                                                {
+                                                    navigation.map((item) => (
+                                                        <li key={item.name} onClick={() => setIsDropdownOpen(false)}>
+                                                            <Link to={item.href} className="block px-4 py-2 text-sm hover:bg-gray-100">
+                                                                {item.name}
+                                                            </Link>
+                                                        </li>
+                                                    ))
+                                                }
+                                                <li>
+                                                    <button
+                                                    onClick={handleLogOut}
+                                                    className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100">Logout</button>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    )
+                                }
+                                </> 
+                            ) : (
+                                // Nếu KHÔNG có currentUser -> Luôn hiển thị icon Login
                                 <Link to="/login"> <HiOutlineUser className="size-6 text-ink" /></Link>
                             )
                         }
